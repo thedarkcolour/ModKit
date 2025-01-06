c575e71bebcacc361ad4b3ff8ecb7335a25b8dc5
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2025-01-05 22:30:30 -0800

    Fix door recipe counts being incorrect

--- src/main/java/thedarkcolour/modkit/data/MKRecipeProvider.java
+++ src/main/java/thedarkcolour/modkit/data/MKRecipeProvider.java
@@ -528,7 +528,7 @@ public class MKRecipeProvider extends RecipeProvider {
     public void grid3x2(RecipeCategory category, ItemLike result, int resultCount, Ingredient ingredient, @Nullable String group) {
         Preconditions.checkNotNull(this.writer);
 
-        shapedCrafting(category, result, recipe -> {
+        shapedCrafting(category, result, resultCount, recipe -> {
             recipe.define('#', ingredient);
             recipe.pattern("###");
             recipe.pattern("###");
