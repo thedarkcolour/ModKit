c0c491b35bf7c5785b933b27966a9c0b9dee4163
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2026-09-07 12:46:51 -0700

    Fix grid2x2 ignoring resultCount parameter

--- src/main/java/thedarkcolour/modkit/data/MKRecipeProvider.java
+++ src/main/java/thedarkcolour/modkit/data/MKRecipeProvider.java
@@ -504,7 +504,7 @@ public class MKRecipeProvider extends RecipeProvider {
     public void grid2x2(RecipeCategory category, ItemLike result, int resultCount, Ingredient ingredient, @Nullable String group) {
         Preconditions.checkNotNull(this.output);
 
-        shapedCrafting(category, result, recipe -> {
+        shapedCrafting(category, result, resultCount, recipe -> {
             recipe.define('#', ingredient);
             recipe.pattern("##");
             recipe.pattern("##");
